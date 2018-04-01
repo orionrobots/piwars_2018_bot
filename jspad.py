a1496f861582a71bcb88a74ea072dd8a67cf735a
Administrator <admin@example.com>
2018-04-01 14:29:20 +0000

    Add joystick reader first attempt - two sides.

---
+++ jspad.py
@@ -0,0 +1,16 @@
+from approxeng.input.selectbinder import ControllerResource
+
+while True:
+    try:
+        with ControllerResource() as joystick:
+            print('Found a joystick and connected')
+            while joystick.connected:
+                # Do stuff with your joystick here!
+                print("Left track", joystick.ly)
+                print("Right track", joystick.ry)
+        # Joystick disconnected...
+        print('Connection to joystick lost')
+    except IOError:
+        # No joystick found, wait for a bit before trying again
+        print('Unable to find any joysticks')
+        sleep(1.0)

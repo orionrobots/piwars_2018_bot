8370b14a82aa2e83208032ba161153940a6727fb
Administrator <admin@example.com>
2018-04-02 12:51:22 +0000

    Add a dualshock tank control test

---
+++ dualshock_tank.py
@@ -0,0 +1,30 @@
+# Picon Zero Dualshock tank drive
+# Press Ctrl-C to stop
+#
+# To check wiring is correct ensure the order of movement as above is correct
+
+import piconzero as pz, time
+
+from approxeng.input.dualshock3 import DualShock3
+from approxeng.input.controllers import find_single_controller
+from approxeng.input.selectbinder import ControllerResource
+
+def setup_controller():
+    devices, controller, p = find_single_controller(controller_class = DualShock3)
+    devices = [devices[0]]
+    return devices, controller, p
+    
+print("Tests the motors by using the dualshock pad")
+
+pz.init()
+try:
+
+    with ControllerResource(devices=devices, controller=controller) as joystick:
+        while joystick.connected:
+            left_track = joystick.ly
+            right_track = joystick.ry
+            pz.setMotor(0, left_track * 100)
+            pz.setMotor(0, right_track * 100)
+
+finally:
+    pz.cleanup()

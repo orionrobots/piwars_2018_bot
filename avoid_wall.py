35df1ce6a9ece74d71336f7fc6a50a79c95103fe
Danny Staple <danny@orionrobots.co.uk>
2018-04-18 22:20:19 +0100

    Modify avoid wall logic

--- avoid_wall.py
+++ avoid_wall.py
@@ -10,18 +10,14 @@ def main():
             forward = robot.forward_distance
             right = robot.right_distance
             print(left, forward, right)
-            robot.set_left(100)
-            robot.set_right(100)
-            if forward < left and forward < right:
-                if left < right:
+            if forward > 8:
+                robot.set_left(100)
+                robot.set_right(100)
+            else:
+                if left > right:
                     robot.set_left(0)
                 else:
                     robot.set_right(0)
-            elif forward < left:
-                robot.set_right(0)
-            elif forward < right:
-                robot.set_left(0)
-
 
 if __name__ == '__main__':
     main()

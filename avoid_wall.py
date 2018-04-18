20ec9f2978299bf43204d2d25c332eacbc8f02df
Danny <danny@orionrobots.co.uk>
2018-04-18 21:49:53 +0000

    Adding tweak values

--- avoid_wall.py
+++ avoid_wall.py
@@ -2,6 +2,9 @@
 from piwars_bot import Robot
 from piwars_bot_controller import PiWarsController
 
+forward_speed = 100
+reverse_speed  = -50
+forward_react_dist = 10
 def main():
     mode = 'Waiting for first reading'
     with Robot() as robot: #, PiWarsController().connect_pad() as joystick:
@@ -11,21 +14,21 @@ def main():
             forward = robot.forward_distance
             right = robot.right_distance
             print(left, forward, right, mode)
-            if mode == 'turning right' and forward < 10:
-                robot.set_right(0)
-            elif mode = = 'turning left' and forward < 10:
-                robot.set_left(0)
-            elif forward > 10:
-                robot.set_left(100)
-                robot.set_right(100)
+            if mode == 'turning right' and forward < forward_react_dist:
+                robot.set_right(reverse_speed)
+            elif mode == 'turning left' and forward < forward_react_dist:
+                robot.set_left(reverse_speed)
+            elif forward > forward_react_dist:
+                robot.set_left(forward_speed)
+                robot.set_right(forward_speed)
                 mode = 'forward'
             else:
                 if left < right:
                     mode = 'turning left'
-                    robot.set_left(0)
+                    robot.set_left(reverse_speed)
                 else:
                     mode = 'turning right'
-                    robot.set_right(0)
+                    robot.set_right(reverse_speed)
 
 if __name__ == '__main__':
     main()

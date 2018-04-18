f9fb625c9f8a63161952d8772c5bc3aaa9c12796
Danny Staple <danny@orionrobots.co.uk>
2018-04-18 21:55:26 +0100

    Tweak wlal avoiding

--- avoid_wall.py
+++ avoid_wall.py
@@ -3,19 +3,22 @@ from piwars_bot import Robot
 from piwars_bot_controller import PiWarsController
 
 def main():
-    with Robot() as robot, PiWarsController().connect_pad() as joystick:
-        while joystick.connected and not joystick['home']:
+    with Robot() as robot: #, PiWarsController().connect_pad() as joystick:
+        while True: #joystick.connected and not joystick['home']:
             # of left, right and forward - which is furthest?
             left = robot.left_distance
             forward = robot.forward_distance
             right = robot.right_distance
-            if forward > left:
-                robot.set_left(100)
-            else:
+            robot.set_left(100)
+            robot.set_right(100)
+            if forward < left and forward < right:
+                if left <  right:
+                    robot.set_left(0)
+                else:
+                    robot.set_right(0)
+            elif forward < left:
                 robot.set_left(0)
-            if forward > right:
-                robot.set_right(100)
-            else:
+            elif forward < right:
                 robot.set_right(0)
 
 
